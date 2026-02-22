574975051e97728930719f5213ce3647edd33947
GRRNebula2 <pahkina.katainen@protonmail.com>
2026-02-22 19:34:39 +0200

    helabaarit, attackit. Hypystä laskeutuminen bugaa.

--- main.js
+++ main.js
@@ -270,6 +270,141 @@ scene("fight", () => {
         destroyAll(player2.id + "attackHitbox")
     })
 
+    const counter = add([
+        rect(100, 100),
+        pos(center().x, center().y - 300),
+        color(10,10,10),
+        area(),
+        anchor("center")
+    ])
+
+    const count = counter.add([
+        text("60"),
+        area(),
+        anchor("center"),
+        {
+            timeLeft: 60,
+        }
+    ])
+
+    const winningText = add([
+        text(""),
+        area(),
+        anchor("center"),
+        pos(center())
+    ])
+
+    let gameOver = false
+    onKeyDown("enter", () => gameOver ? go("fight") : null)
+
+    function declareWinner(winningText, player1, player2) {
+        if (player1.health > 0 && player2.health > 0
+            || player1.health === 0 && player2.health === 0) {
+                winningText.text = "Tie!"
+            }
+            else if (player1.health > 0 && player2.health === 0) {
+                winningText.text = "Player 1 won!"
+                player2.use(sprite(player2.sprites.death))
+                player2.play("death")
+            }
+            else {
+                winningText.text = "Player 2 won"
+                player1.use(sprite(player1.sprites.death))
+                player1.play("death")
+            }
+    }
+
+    const countInterval = setInterval(() => {
+        if (count.timeLeft === 0) {
+            clearInterval(countInterval)
+            declareWinner(winningText, player1, player2)
+            gameOver = true
+
+            return
+        }
+
+        count.timeLeft--
+        count.text = count.timeLeft
+    }, 1000)
+
+    const player1HealthContainer = add([
+        rect(500, 70),
+        area(),
+        outline(5),
+        pos(90,20),
+        color(200, 0, 0)
+    ])
+
+    const player1HealthBar = player1HealthContainer.add([
+        rect(498, 65),
+        color(0, 180, 0),
+        pos(498, 70 - 2.5),
+        rotate(180)
+    ])
+
+    player1.onCollide(player2.id + "attackHitbox", () => {
+        if (gameOver) {
+            return
+        }
+
+        if(player1.health !== 0) {
+            player1.health -= 50
+            tween(
+                player1HealthBar.width,
+                player1.health,
+                1,
+                (val) => { player1HealthBar.width = val},
+                easings.easeOutSine
+            )
+        }
+
+        if (player1.health === 0) {
+            clearInterval(countInterval)
+            declareWinner(winningText, player1, player2)
+            gameOver = true
+        }
+
+    })
+
+    const player2HealthContainer = add([
+        rect(500, 70),
+        area(),
+        outline(5),
+        pos(690, 20),
+        color(200, 0, 0)
+    ])
+
+    const player2HealthBar = player2HealthContainer.add([
+        rect(498, 65),
+        color(0, 180, 0),
+        pos(2.5, 2.5)
+    ])
+
+    player2.onCollide(player1.id + "attackHitbox", () => {
+        if (gameOver) {
+            return
+        }
+
+        if (player2.health !== 0) {
+            player2.health -= 50
+            tween(
+                player2HealthBar.width,
+                player2.health,
+                1,
+                (val) => {player2HealthBar.width = val},
+                easings.easeOutSine
+            )
+        }
+
+        if (player2.health === 0) {
+            clearInterval(countInterval)
+            declareWinner(winningText, player1, player2)
+            gameOver = true
+        }
+
+
+    })
+
 })
 
 
