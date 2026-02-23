9eb81b0ca36849739cd5dd77f73592f2d1840d28
GRRNebula2 <pahkina.katainen@protonmail.com>
2026-02-23 21:03:24 +0200

    sulkuja ja puolipisteitä by vs code or formatter

--- entities/Player.js
+++ entities/Player.js
@@ -1,166 +1,151 @@
-/*export class Player {
+export class Player {
+  constructor(positionX, positionY, speed, left, right, jump, attackKey, id) {
+    (this.positionX = positionX),
+      (this.positionY = positionY),
+      (this.speed = speed);
+    (this.left = left),
+      (this.right = right),
+      (this.jump = jump),
+      (this.attackKey = attackKey),
+      (this.id = id);
+    this.makePlayer();
+    this.setPlayerMovement();
+  }
 
-constructor(
-    positionX,
-    positionY,
-    speed,
-    left,
-    right,
-    jump,
-    attackKey,
-    id
-    ) {
-    this.positionX = positionX,
-    this.positionY = positionY,
-    this.speed = speed
-    this.left = left,
-    this.right = right,
-    this.jump = jump,
-    this.attackKey = attackKey,
-    this.id = id
-    this.makePlayer()
-    this.setPlayerMovement()
-}
-
-makePlayer() {
+  makePlayer() {
     add([
-        pos(this.positionX, this.positionY),
-        scale(4),
-        area({shape: new Rect(vec2(0), 16, 42)}),
-        anchor("center"),
-        body({stickToPlatform: true}),
-        {
-            isCurrentlyJumping: false,
-            health: 500,
-            sprites: {
-                run: "run-" + this.id,
-                idle: "idle-" + this.id,
-                jump: "jump-" + this.id,
-                attack: "attack-" + this.id,
-                death: "death-" + this.id
-            }
-        }
-    ])
-}
+      pos(this.positionX, this.positionY),
+      scale(4),
+      area({ shape: new Rect(vec2(0), 16, 42) }),
+      anchor("center"),
+      body({ stickToPlatform: true }),
+      {
+        isCurrentlyJumping: false,
+        health: 500,
+        sprites: {
+          run: "run-" + this.id,
+          idle: "idle-" + this.id,
+          jump: "jump-" + this.id,
+          attack: "attack-" + this.id,
+          death: "death-" + this.id,
+        },
+      },
+    ]);
+  }
 
-setPlayerMovement() {
+  setPlayerMovement() {
+    run(speed, flipPlayer, () => {
+      if (player.health === 0) {
+        return;
+      }
 
-run(speed, flipPlayer, () => {
-    if (player.health === 0) {
-        return
-    }
+      if (player.curAnim() !== "run" && !player.isCurrentlyJumping) {
+        player.use(sprite(player.sprites.run));
+        player.play("run");
+      }
+      player.move(speed, 0);
+      player.flipX = flipPlayer;
+    });
 
-    if (player.curAnim() !== "run"
-    && !player.isCurrentlyJumping) {
-        player.use(sprite(player.sprites.run))
-        player.play("run")
-    }
-    player.move(speed, 0)
-    player.flipX = flipPlayer
-})
+    resetPlayerToIdle(() => {
+      player.use(sprite(player.sprites.idle));
+      player.play("idle");
+    });
 
-resetPlayerToIdle(() => {
-    player.use(sprite(player.sprites.idle))
-    player.play("idle")
-})
+    onKeyDown(this.right, () => {
+      run(this.speed, false);
+    });
 
-onKeyDown(this.right, () => {
-    run(this.speed, false)
-})
+    onKeyRelease(this.right, () => {
+      if (player.health !== 0) {
+        resetPlayerToIdle();
+        player.flipX = false;
+      }
+    });
 
-onKeyRelease(this.right, () => {
-    if (player.health !== 0) {
-        resetPlayerToIdle()
-        player.flipX = false
-    }
-})
+    onKeyDown(this.left, () => {
+      run(-this.speed, true);
+    });
 
-onKeyDown(this.left, () => {
-    run(-this.speed, true)
-})
+    onKeyRelease(this.left, () => {
+      if (player.health !== 0) {
+        resetPlayerToIdle(player1);
+        player.flipX = true;
+      }
+    });
 
-onKeyRelease(this.left, () => {
-    if(player.health !== 0) {
-        resetPlayerToIdle(player1)
-        player.flipX = true
-    }
-})
+    makeJump(() => {
+      if (player.health === 0) {
+        return;
+      }
 
-makeJump( () => {
-    if (player.health === 0) {
-        return
-    }
+      if (player.isGrounded()) {
+        const currentFlip = player.flipX;
+        player.jump();
+        player.use(sprite(player.sprites.jump));
+        player.flipX = currentFlip;
+        player.play("jump");
+        player.isCurrentlyJumping = true;
+      }
+    });
 
-    if (player.isGrounded()) {
-        const currentFlip = player.flipX
-        player.jump()
-        player.use(sprite(player.sprites.jump))
-        player.flipX = currentFlip
-        player.play("jump")
-        player.isCurrentlyJumping = true
-    }
-})
-
-resetAfterJump(() => {
-    if (player.isGrounded() && player.isCurrentlyJumping) {
-        player.isCurrentlyJumping = false
+    resetAfterJump(() => {
+      if (player.isGrounded() && player.isCurrentlyJumping) {
+        player.isCurrentlyJumping = false;
         if (player.curAnim() !== "idle") {
-            resetPlayerToIdle(player)
+          resetPlayerToIdle(player);
         }
-    }
-})
+      }
+    });
 
-onKeyDown(this.up, () => {
-    makeJump()
-})
+    onKeyDown(this.up, () => {
+      makeJump();
+    });
 
-onUpdate(() => resetAfterJump())
+    onUpdate(() => resetAfterJump());
 
+    attack(excludedKeys, () => {
+      if (player.health === 0) {
+        return;
+      }
 
-attack(excludedKeys, () => {
-    if (player.health === 0) {
-        return  
-    }
-
-    for (const key of excludedKeys) {
+      for (const key of excludedKeys) {
         if (isKeyDown(key)) {
-            return
+          return;
         }
-    }
+      }
 
-    const currentFlip = player.flipX
-    if (player.curAnim() !== "attack") {
-        player.use(sprite(player.sprites.attack))
-        player.flipX = currentFlip
-        const slashX = player.pos.x + 30
-        const slashXFlipped = player.pos.x - 350
-        const slashY = player.pos.y - 200
+      const currentFlip = player.flipX;
+      if (player.curAnim() !== "attack") {
+        player.use(sprite(player.sprites.attack));
+        player.flipX = currentFlip;
+        const slashX = player.pos.x + 30;
+        const slashXFlipped = player.pos.x - 350;
+        const slashY = player.pos.y - 200;
 
         add([
-            rect(300, 300),
-            area(),
-            pos(currentFlip ? slashXFlipped: slashX, slashY),
-            opacity(0),
-            player.id +"attackHitbox"
-        ])
-        
+          rect(300, 300),
+          area(),
+          pos(currentFlip ? slashXFlipped : slashX, slashY),
+          opacity(0),
+          player.id + "attackHitbox",
+        ]);
+
         player.play("attack", {
-            onEnd: () => {
-                resetPlayerToIdle(player)
-                player.flipX = currentFlip
-            }
-        })
-    }
+          onEnd: () => {
+            resetPlayerToIdle(player);
+            player.flipX = currentFlip;
+          },
+        });
+      }
+    });
 
-})
+    onKeyPress(this.attackKey, () => {
+      attack([this.left, this.right, this.jump]);
+    });
 
-onKeyPress(this.attackKey, () => {
-    attack([this.left, this.right, this.jump])
-})
-
-onKeyRelease(this.attackKey, () => {
-    destroyAll(player.id + "attackHitbox")
-})
+    onKeyRelease(this.attackKey, () => {
+      destroyAll(player.id + "attackHitbox");
+    });
+  }
 }
-
-} */
