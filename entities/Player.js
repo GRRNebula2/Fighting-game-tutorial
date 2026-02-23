d39351cb990686285701864b7eec90aa64375974
GRRNebula2 <pahkina.katainen@protonmail.com>
2026-02-23 14:38:00 +0200

    Siirto mainista Player-luokkaan. Vielä kasa bugeja.

---
+++ entities/Player.js
@@ -0,0 +1,159 @@
+constructor(
+positionX,
+positionY,
+speed,
+left,
+right,
+jump,
+attackKey,
+id
+) 
+{
+    this.positionX = positionX,
+    this.positionY = positionY,
+    this.speed = speed
+    this.left = left,
+    this.right = right,
+    this.jump = jump,
+    this.attackKey = attackKey,
+    this.id = id
+    this.makePlayer(positionX, positionY, id)
+}
+
+makePlayer(posX, posY, id) {
+        add([
+        pos(posX, posY),
+        scale(4),
+        area({shape: new Rect(vec2(0), 16, 42)}),
+        anchor("center"),
+        body({stickToPlatform: true}),
+        {
+            isCurrentlyJumping: false,
+            health: 500,
+            sprites: {
+                run: "run-" + id,
+                idle: "idle-" + id,
+                jump: "jump-" + id,
+                attack: "attack-" + id,
+                death: "death-" + id
+            }
+        }
+    ])
+}
+
+function run(speed, flipPlayer) {
+    if (player.health === 0) {
+        return
+    }
+
+    if (player.curAnim() !== "run"
+    && !player.isCurrentlyJumping) {
+        player.use(sprite(player.sprites.run))
+        player.play("run")
+    }
+    player.move(speed, 0)
+    player.flipX = flipPlayer
+}
+
+function resetPlayerToIdle() {
+    player.use(sprite(player.sprites.idle))
+    player.play("idle")
+}
+
+onKeyDown(this.right, () => {
+    run(this.speed, false)
+})
+onKeyRelease(this.right, () => {
+    if (player.health !== 0) {
+        resetPlayerToIdle()
+        player.flipX = false
+    }
+})
+
+onKeyDown(this.left, () => {
+    run(-this.speed, true)
+})
+
+onKeyRelease(this.left, () => {
+    if(player.health !== 0) {
+        resetPlayerToIdle(player1)
+        player.flipX = true
+    }
+})
+
+makeJump() {
+    if (player.health === 0) {
+        return
+    }
+
+    if (player.isGrounded()) {
+        const currentFlip = player.flipX
+        player.jump()
+        player.use(sprite(player.sprites.jump))
+        player.flipX = currentFlip
+        player.play("jump")
+        player.isCurrentlyJumping = true
+    }
+}
+
+resetAfterJump() {
+    if (player.isGrounded() && player.isCurrentlyJumping) {
+        player.isCurrentlyJumping = false
+        if (player.curAnim() !== "idle") {
+            resetPlayerToIdle(player)
+        }
+    }
+}
+
+onKeyDown(this.up, () => {
+    makeJump()
+})
+
+
+onUpdate(() => resetAfterJump())
+
+function attack(excludedKeys) {
+    if (player.health === 0) {
+        return
+    }
+
+    for (const key of excludedKeys) {
+        if (isKeyDown(key)) {
+            return
+        }
+    }
+
+    const currentFlip = player.flipX
+    if (player.curAnim() !== "attack") {
+        player.use(sprite(player.sprites.attack))
+        player.flipX = currentFlip
+        const slashX = player.pos.x + 30
+        const slashXFlipped = player.pos.x - 350
+        const slashY = player.pos.y - 200
+
+        add([
+            rect(300, 300),
+            area(),
+            pos(currentFlip ? slashXFlipped: slashX, slashY),
+            opacity(0),
+            player.id +"attackHitbox"
+        ])
+        
+        player.play("attack", {
+            onEnd: () => {
+                resetPlayerToIdle(player)
+                player.flipX = currentFlip
+            }
+        })
+    }
+
+}
+
+onKeyPress(this.attackKey, () => {
+    attack([this.left, this.right, this.jump])
+})
+
+onKeyRelease(this.attackKey, () => {
+    destroyAll(player.id + "attackHitbox")
+})
+
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs"
 import { load } from "./utils/loader.js"
+import { Player } from "./entities/Player.js"
 
 //import kaboom from "./libs/kaboom.mjs"
 
@@ -88,187 +89,40 @@ scene("fight", () => {
         pos(290, 115)
        ])
     
-    
-       function makePlayer(posX, posY, width, height, scaleFactor, id) {
-        return add([
-            pos(posX, posY),
-            scale(scaleFactor),
-            area({shape: new Rect(vec2(0), width, height)}),
-            anchor("center"),
-            body({stickToPlatform: true}),
-            {
-                isCurrentlyJumping: false,
-                health: 500,
-                sprites: {
-                    run: "run-" + id,
-                    idle: "idle-" + id,
-                    jump: "jump-" + id,
-                    attack: "attack-" + id,
-                    death: "death-" + id
-                }
-            }
-        ])
-    }
+    const player1 = new Player(
+        200,
+        100,
+        500,
+        "a",
+        "d",
+        "w",
+        "space",
+        "player1"
+    )
 
     setGravity(1200)
 
-    const player1 = makePlayer(200, 100, 16, 42, 4, "player1")
     player1.use(sprite(player1.sprites.idle))
     player1.play("idle")
 
-    function run(player, speed, flipPlayer) {
-        if (player.health === 0) {
-            return
-        }
-
-        if (player.curAnim() !== "run"
-        && !player.isCurrentlyJumping) {
-            player.use(sprite(player.sprites.run))
-            player.play("run")
-        }
-        player.move(speed, 0)
-        player.flipX = flipPlayer
-    }
-
-    function resetPlayerToIdle(player) {
-        player.use(sprite(player.sprites.idle))
-        player.play("idle")
-    }
-
-    onKeyDown("d", () => {
-        run(player1, 500, false)
-    })
-    onKeyRelease("d", () => {
-        if (player1.health !== 0) {
-            resetPlayerToIdle(player1)
-            player1.flipX = false
-        }
-    })
-
-    onKeyDown("a", () => {
-        run(player1, -500, true)
-    })
-
-    onKeyRelease("a", () => {
-        if(player1.health !== 0) {
-            resetPlayerToIdle(player1)
-            player1.flipX = true
-        }
-    })
-
-    function makeJump(player) {
-        if (player.health === 0) {
-            return
-        }
-
-        if (player.isGrounded()) {
-            const currentFlip = player.flipX
-            player.jump()
-            player.use(sprite(player.sprites.jump))
-            player.flipX = currentFlip
-            player.play("jump")
-            player.isCurrentlyJumping = true
-        }
-    }
-
-    function resetAfterJump(player) {
-        if (player.isGrounded() && player.isCurrentlyJumping) {
-            player.isCurrentlyJumping = false
-            if (player.curAnim() !== "idle") {
-                resetPlayerToIdle(player)
-            }
-        }
-    }
-
-    onKeyDown("w", () => {
-        makeJump(player1)
-    })
-
     player1.onUpdate(() => resetAfterJump(player1))
 
-    function attack(player, excludedKeys) {
-        if (player.health === 0) {
-            return
-        }
+    const player2 = new Player(
+        1000,
+        200,
+        500,
+        "left",
+        "right",
+        "up",
+        "down",
+        "player2"
+    )
 
-        for (const key of excludedKeys) {
-            if (isKeyDown(key)) {
-                return
-            }
-        }
-
-        const currentFlip = player.flipX
-        if (player.curAnim() !== "attack") {
-            player.use(sprite(player.sprites.attack))
-            player.flipX = currentFlip
-            const slashX = player.pos.x + 30
-            const slashXFlipped = player.pos.x - 350
-            const slashY = player.pos.y - 200
-
-            add([
-                rect(300, 300),
-                area(),
-                pos(currentFlip ? slashXFlipped: slashX, slashY),
-                opacity(0),
-                player.id +"attackHitbox"
-            ])
-            
-            player.play("attack", {
-                onEnd: () => {
-                    resetPlayerToIdle(player)
-                    player.flipX = currentFlip
-                }
-            })
-        }
-
-    }
-
-    onKeyPress("space", () => {
-        attack(player1, ["a", "d", "w"])
-    })
-
-    onKeyRelease("space", () => {
-        destroyAll(player1.id + "attackHitbox")
-    })
-
-    const player2 = makePlayer(1000, 200, 16, 52, 4, "player2")
     player2.use(sprite(player2.sprites.idle))
     player2.play("idle")
-    player2.flipX
+    player2.flipX = true
 
-    onKeyDown("right", () => {
-        run(player2, 500, false)
-    })
-
-    onKeyRelease("right", () => {
-        if (player2.health !== 0) {
-            resetPlayerToIdle(player2)
-            player2.flipX
-        }
-    })
-
-    onKeyDown("left", () => {
-        run(player2, -500, true)
-    })
-
-    onKeyRelease("left", () => {
-        resetPlayerToIdle(player2)
-        player2.flipX = true
-    })
-
-    onKeyDown("up", () => {
-        makeJump(player2)
-    })
-
-    player2.onUpdate(() => resetAfterJump(player2))
-
-    onKeyPress("down", () => {
-        attack(player2, ["left", "right", "up"])
-    })
-
-    onKeyRelease("down", () => {
-        destroyAll(player2.id + "attackHitbox")
-    })
+    player2.onUpdate(() => resetAfterJump())
 
     const counter = add([
         rect(100, 100),
